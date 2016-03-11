75f73284965cdb28c6fd80945df306cc8127f304
Karl Westin <karl.westin@gmail.com>
2016-03-11 17:39:22 +0100

    block actions after game ended, add new-game button

--- src/cljs/cljs_minesweeper/core.cljs
+++ src/cljs/cljs_minesweeper/core.cljs
@@ -21,17 +21,25 @@
   (let [currentboard @state]
     (reset! state (game/mark currentboard col row))))
 
+(defn new-game []
+  (reset! state (game/board 10 10 10)))
+
 (defn game []
   (let [board @state
         won (game/won? board)
         lost (game/lost? board)
+        ended (or won lost)
+        click-fn (if ended #() click)
+        mark-fn (if ended #() mark)
         title (cond
-                won "You Swept the Commies!"
+                won "You Swept the Commies! Freedom!!$$$"
                 lost "The Commies Won!!"
-                :else "ClojureScript Commie Sweeper")]
+                :else "Fight Communism with ClojureScript!")]
     [:div
       [:h1 title]
-      [components/board @state click mark]]))
+      [components/board @state click-fn mark-fn]
+      (when ended
+        [:button {:on-click new-game} "lets try to beat them again"])]))
 
 (reagent/render
   [game]
